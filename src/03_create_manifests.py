7f954905eb8b9964f98f114959f38c22110d7a5b
nakamura196 <na.kamura.1263@gmail.com>
2021-01-27 00:57:39 +0900

    fix: order

--- src/03_create_manifests.py
+++ src/03_create_manifests.py
@@ -24,159 +24,168 @@ manifests = []
 
 prefix0 = "https://shibusawa-dlab.github.io/lab1/iiif/"
 
+data_map = {}
+
 for ad in df:
     if "http://schema.org/url" in ad:
-        url = ad["http://schema.org/url"][0]["@id"]
 
-        print(url)
 
-        name = url.split("/")[-1]
+        data_map[ad["@id"]] = ad
 
-        path = "data/html/" + name + ".html"
+for id in sorted(data_map):
+    ad = data_map[id]
 
-        if not os.path.exists(path):
-            r = requests.get(url)
-            soup = BeautifulSoup(r.content)
+    url = ad["http://schema.org/url"][0]["@id"]
 
-            with open(path, mode='w') as f:
-                f.write(str(soup))
+    print(url)
 
-        soup = BeautifulSoup(open(path,'r'), "lxml")
+    name = url.split("/")[-1]
 
-        # print(soup)
+    path = "data/html/" + name + ".html"
 
-        arr = str(soup).split("'")
+    if not os.path.exists(path):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content)
 
-        imgs = []
+        with open(path, mode='w') as f:
+            f.write(str(soup))
 
-        for a in arr:
-            if "http://base1.nijl.ac.jp/~jituhaku" in a and "\"" not in a and a not in imgs:
-                imgs.append(a)
+    soup = BeautifulSoup(open(path,'r'), "lxml")
 
-        print(imgs)
+    # print(soup)
 
-        canvases = []
+    arr = str(soup).split("'")
 
-        thumbnail = {}
+    imgs = []
 
-        prefix = "{}{}".format(prefix0, name)
-        manifest_uri = prefix + "/manifest.json".format(name)
+    for a in arr:
+        if "http://base1.nijl.ac.jp/~jituhaku" in a and "\"" not in a and a not in imgs:
+            imgs.append(a)
 
-        for i in range(len(imgs)):
+    print(imgs)
 
-            img = imgs[i]
+    canvases = []
 
-            index = i + 1
+    thumbnail = {}
 
-            name2 = img.split("/")[-1]
+    prefix = "{}{}".format(prefix0, name)
+    manifest_uri = prefix + "/manifest.json".format(name)
 
-            path2 = "data/images/"+name2
+    for i in range(len(imgs)):
 
-            if not os.path.exists(path2):
+        img = imgs[i]
 
-                r = requests.get(img, stream=True)
-                if r.status_code == 200:
-                    with open(path2, 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
+        index = i + 1
 
-            
-            im = Image.open(path2)
+        name2 = img.split("/")[-1]
 
-            w,h = im.size
+        path2 = "data/images/"+name2
 
-            print(w,h )
+        if not os.path.exists(path2):
 
-            canvases.append({
-                "@id": "{}/canvas/p{}".format(prefix, index),
-                "@type": "sc:Canvas",
-                "height": h,
-                "images": [
-                    {
-                    "@id": "{}/annotation/p{}-image".format(prefix, index),
-                    "@type": "oa:Annotation",
-                    "motivation": "sc:painting",
-                    "on": "{}/canvas/p{}".format(prefix, index),
-                    "resource": {
-                        "@id": img,
-                        "@type": "dctypes:Image",
-                        "format": "image/jpeg",
-                        "height": h,
-                        "width": w
-                    }
-                    }
-                ],
-                "label": "[{}]".format(index),
-                "thumbnail": {
-                    "@id": img,
-                    "@type": "dctypes:Image",
-                    "format": "image/jpeg",
-                    "width": w,
-                    "height": h
-                },
-                "width": w
-                })
+            r = requests.get(img, stream=True)
+            if r.status_code == 200:
+                with open(path2, 'wb') as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
 
-            if i == 0:
-                thumbnail = {
-                    "@id": img,
-                    "@type": "dctypes:Image",
-                    "format": "image/jpeg",
-                    "width": w,
-                    "height": h
-                }
-
-        trs = soup.find(class_="detail_tbl").find("tbody").find_all("tr")
-        metadata = []
-        for tr in trs:
-            tds = tr.find_all("td")
-            print(tds)
-            metadata.append({
-                "label" : tds[0].text,
-                "value" : tds[1].text.strip()
-            })
         
-        
-        manifest = {
-            "@context": "http://iiif.io/api/presentation/2/context.json",
-            "@id": manifest_uri,
-            "@type": "sc:Manifest",
-            "attribution": "国文学研究資料館",
-            "label": soup.find(class_="infolib_section").text.strip(),
-            "license": "http://base1.nijl.ac.jp/~jituhaku/",
-            "metadata": metadata,
-            "thumbnail" : thumbnail,
-            "related": {
-                "@id" : url,
-                "format": "text/html"
-            },
-            "sequences": [
+        im = Image.open(path2)
+
+        w,h = im.size
+
+        print(w,h )
+
+        canvases.append({
+            "@id": "{}/canvas/p{}".format(prefix, index),
+            "@type": "sc:Canvas",
+            "height": h,
+            "images": [
                 {
-                    "@id": "{}/sequence/normal".format(prefix),
-                    "@type": "sc:Sequence",
-                    "canvases": canvases
+                "@id": "{}/annotation/p{}-image".format(prefix, index),
+                "@type": "oa:Annotation",
+                "motivation": "sc:painting",
+                "on": "{}/canvas/p{}".format(prefix, index),
+                "resource": {
+                    "@id": img,
+                    "@type": "dctypes:Image",
+                    "format": "image/jpeg",
+                    "height": h,
+                    "width": w
+                }
                 }
             ],
-            "viewingDirection": "right-to-left"
-        }
+            "label": "[{}]".format(index),
+            "thumbnail": {
+                "@id": img,
+                "@type": "dctypes:Image",
+                "format": "image/jpeg",
+                "width": w,
+                "height": h
+            },
+            "width": w
+            })
 
-        manifests.append({
-            # "@context": "http://iiif.io/api/presentation/2/context.json",
-            "@id": manifest["@id"],
-            "@type": "sc:Manifest",
-            "label": manifest["label"],
-            "thumbnail": thumbnail["@id"]
+        if i == 0:
+            thumbnail = {
+                "@id": img,
+                "@type": "dctypes:Image",
+                "format": "image/jpeg",
+                "width": w,
+                "height": h
+            }
+
+    trs = soup.find(class_="detail_tbl").find("tbody").find_all("tr")
+    metadata = []
+    for tr in trs:
+        tds = tr.find_all("td")
+        print(tds)
+        metadata.append({
+            "label" : tds[0].text,
+            "value" : tds[1].text.strip()
         })
+    
+    
+    manifest = {
+        "@context": "http://iiif.io/api/presentation/2/context.json",
+        "@id": manifest_uri,
+        "@type": "sc:Manifest",
+        "attribution": "国文学研究資料館",
+        "label": soup.find(class_="infolib_section").text.strip(),
+        "license": "http://base1.nijl.ac.jp/~jituhaku/",
+        "metadata": metadata,
+        "thumbnail" : thumbnail,
+        "related": {
+            "@id" : url,
+            "format": "text/html"
+        },
+        "sequences": [
+            {
+                "@id": "{}/sequence/normal".format(prefix),
+                "@type": "sc:Sequence",
+                "canvases": canvases
+            }
+        ],
+        "viewingDirection": "right-to-left"
+    }
 
-        path = "../static/iiif/{}/manifest.json".format(name)
+    manifests.append({
+        # "@context": "http://iiif.io/api/presentation/2/context.json",
+        "@id": manifest["@id"],
+        "@type": "sc:Manifest",
+        "label": manifest["label"],
+        "thumbnail": thumbnail["@id"]
+    })
 
-        dir = os.path.dirname(path)
+    path = "../static/iiif/{}/manifest.json".format(name)
 
-        os.makedirs(dir, exist_ok=True)
+    dir = os.path.dirname(path)
 
-        with open(path, 'w') as outfile:
-            json.dump(manifest,  outfile, ensure_ascii=False,
-                indent=4, sort_keys=True, separators=(',', ': '))
+    os.makedirs(dir, exist_ok=True)
+
+    with open(path, 'w') as outfile:
+        json.dump(manifest,  outfile, ensure_ascii=False,
+            indent=4, sort_keys=True, separators=(',', ': '))
 
 collection = {
   "@context": "http://iiif.io/api/presentation/2/context.json",
